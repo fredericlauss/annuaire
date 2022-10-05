4adc77b132cb06c050039d611a343d3f73bafce9
frederic <flausson@normandiewebschool.fr>
2022-10-05 10:55:01 +0200

    fix variable pour log

--- index.php
+++ index.php
@@ -6,3 +6,6 @@ $connectDb = new Database();
 $connectDb->json();
 $connectDb->DB();
 var_dump($connectDb->host);
+var_dump($connectDb->dbname);
+var_dump($connectDb->login);
+var_dump($connectDb->pwd);
--- services/connectDb.php
+++ services/connectDb.php
@@ -2,10 +2,16 @@
 class Database 
 {
     public  $host;
+    public  $dbname;
+    public  $login;
+    public  $pwd;
 
     public function json()
     {
-        $this->host = json_decode(file_get_contents('services\connect.json'));
+        $this->host = json_decode(file_get_contents('services\connect.json'), true)["HOST"];
+        $this->dbname = json_decode(file_get_contents('services\connect.json'), true)["DBNAME"];
+        $this->login = json_decode(file_get_contents('services\connect.json'), true)["LOGIN"];
+        $this->pwd = json_decode(file_get_contents('services\connect.json'), true)["PWD"];
     }
 
     public static function DB()
