c26533d8f44a2f2c9578808af23d5ab6a21684e2
frederic <flausson@normandiewebschool.fr>
2022-11-03 18:41:38 +0100

    fix du creat des jpo

--- controller/JpoCreatController.php
+++ controller/JpoCreatController.php
@@ -1,12 +1,9 @@
 <?php
 chdir('../');
 require_once('./services/connectDB.php');
-require_once('./manager/BaseManager.php');
-require_once('./manager/JpoStudentManager.php');
 require_once('./manager/JpoManager.php');
-$jpoDeleteController = new StudentDeleteController();
-$jpoDeleteController->deleteJpoStudent();
-$jpoDeleteController->delete();
+$studentCreatController = new JpoCreatController();
+$studentCreatController->creat();
 header('Location: http://localhost/annuaire/jpo.php');
 exit();
 
